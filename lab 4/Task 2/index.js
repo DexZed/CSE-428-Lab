292de6878eece3f345952618734e79367857f891
DexZed <144045080+DexZed@users.noreply.github.com>
2024-03-18 12:33:33 +0600

    changes to index.html and index.js of lab 4 task 2

--- lab 4/Task 2/index.js
+++ lab 4/Task 2/index.js
@@ -1,26 +1,10 @@
+function validation() {
+    let fname = document.getElementById("#fname").value;
+    let lname = document.getElementById("#lname").value;
+    let name = /^[a-zA-Z]{8,10}$/i ;
 
+    if (fname.match(name) === false) {
+        fname.inputElement.placeholder ="The Entered name is not Valid";
+        } 
 
-function valid() {
-    var name = document.getElementById("#name").value;
-    var uname = document.getElementById("#uname").value;
-    var password = document.getElementById("password").value;
-    var gender = document.getElementById("#gender").value;
-    var contact = document.getElementById("#contact").value;
-    let my_notif = document.getElementById("notif");
-
-
-    if (name == "") {
-        document.getElementById("#item1").innerHTML = "Name Cannot be empty";
-    }
-    
-    let vUname = /^[\w\d!@#$%^&*()-+=~`\[\]{}|;:'",.<>?\\/]{8,}$/.exec(uname);
-
-    if (vUname == null || vUname == "") {
-        document.getElementById("#item2").innerHTML = "Userame is not valid";
-    }
-    let mobile = /^\+?(88)?0?1[3456789][0-9]{8}\b/i;
-    let valid_mobile = contact.match(mobile);
-    if (valid_mobile) {
-        my_notif.innerHTML = "Not a Valid Contact";
-    }
 }
